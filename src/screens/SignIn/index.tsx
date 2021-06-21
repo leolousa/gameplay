3e73cde6bb30000a64cec503c220d8c441e275c6
leolousa <leolousa@hotmail.com>
2021-06-20 22:46:49 -0300

    Primeiro commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar'
+import { View, Text } from 'react-native';
+
+export default function SignIn(){
+  return(
+    <View>
+      <StatusBar style="auto"/>
+      <Text>Testando</Text>
+    </View>
+  );
+}
