f458ec41cdafbb20d91a93fa697557993ac1ee55
leolousa <leolousa@hotmail.com>
2021-06-24 19:20:43 -0300

    Criação dos componentes Button, TextArea, Guild e Guilds

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -16,17 +16,5 @@ export const styles = StyleSheet.create({
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: 'center'
-  },
-  iconWrapper: {
-    width: 56,
-    height: 56,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRightWidth: 1,
-    borderColor: theme.colors.line
-  },
-  icon: {
-    width: 24,
-    height: 18
   }
 })
